2ad49ecb1f99ed9e36182ee12c57c75e2ac95584
Sandeep More <moresandeep919@gmail.com>
2023-01-19 22:09:38 +0530

    performed CURD operation using various status code

---
+++ server/db.js
@@ -0,0 +1,44 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize("rapdb", "appdev", "dev123", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+async function connect() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+const synDb = () => {
+  connect();
+  sequelize
+    .sync()
+    .then(() => {
+      console.log("Scan table created successfully!");
+    })
+    .catch((error) => {
+      console.error("Unable to create table : ", error);
+    });
+};
+
+synDb();
+
+export const ScanModel = sequelize.define(
+  "Scans",
+  {
+    // Model attributes are defined here
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+1;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,54 @@
 import express from "express";
-
+import { ScanModel } from "./db.js";
 // create an instance of express.
 const app = express();
 
 const port = 3000;
 
-// app responds with “Hello World!” for requests to the root URL (/) or route
-app.get("/", (req, res) => {
-  res.send("Hello world");
+// req and res should be json
+app.use(express.json());
+
+// app responds with fecth all records from scans table for requests to the root URL (/) or route
+app.get("/", async (req, res) => {
+  const result = await ScanModel.findAll();
+  res.send(result);
+});
+
+app.get("/:id", async (req, res) => {
+  console.log(req.params);
+  const result = await ScanModel.findAll({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.send(result);
+});
+
+// create a resource
+app.post("/", async (req, res) => {
+  const result = await ScanModel.create({ url: req?.body?.url });
+  res.status(201).send(result);
+});
+
+app.put("/:id", async (req, res) => {
+  const result = await ScanModel.update(
+    { url: req?.body?.url },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  res.send(result);
+});
+
+app.delete("/:id", async (req, res) => {
+  const result = await ScanModel.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.sendStatus(204);
 });
 
 // app starts a server and listens on port 3000 for connections.
